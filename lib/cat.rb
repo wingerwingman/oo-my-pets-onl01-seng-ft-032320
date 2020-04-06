54a2d78adfd7508777d8f6ac23615f48509ce750
Justin Winger <justinwinger4@gmail.com>
2020-04-06 22:20:38 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,15 @@
 class Cat
   attr_accessor :mood, :owner
   attr_accessor :name
+  
+  @@all = []
   def initialize(name, owner)
     @name = name
     @mood = "nervous"
     @owner = owner
+  end
+  
+  def self.all 
+    @@all
   end
 end
