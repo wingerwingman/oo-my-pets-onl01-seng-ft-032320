2da49bde0512deb2d0ec57be78fbe07937c57bf6
Justin Winger <justinwinger4@gmail.com>
2020-04-06 22:46:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,7 +64,7 @@ class Owner
   end
   
   def list_pets
-      "I have #{self.cats.count} cat(s), and #{self.cats.count} cat(s)."
+      "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
   end
   
   def say_species
