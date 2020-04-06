3bb6a37a6a336f32702c36fc058903c5fe5f1b96
Justin Winger <justinwinger4@gmail.com>
2020-04-06 22:20:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,15 @@
 class Dog
   attr_accessor :mood, :owner
   attr_reader :name
+  
+  @@all = []
   def initialize(name, owner)
     @name = name
     @mood = "nervous"
     @owner = owner
+  end
+  
+  def self.all
+    @@all
   end
 end
