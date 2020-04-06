5d08a0ba5cbd70db8983991eddbff35b298e6974
Justin Winger <justinwinger4@gmail.com>
2020-04-06 21:19:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,11 @@
 class Owner
   attr_accessor :owner, :name
-  def initialize(owner)
+  def initialize(owner, name)
     @owner = owner 
+    @name = name
   end
   
-  def name(name)
-    self.owner.name = name
-  end
+  # def name(name)
+  #   self.owner.name = name
+  # end
 end
