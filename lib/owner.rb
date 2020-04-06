b870c9a571130d65637714153addefb72ceb9246
Justin Winger <justinwinger4@gmail.com>
2020-04-06 21:35:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Owner
   attr_reader :species
   attr_accessor :name, :pets 
   
   @@all = []
   def initialize(species)
+    binding.pry
     @species = species
     @@all << self
   end
